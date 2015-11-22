1e0a02c933f01d924b20215aa1b36617165ae7e8
Louis Garman <louisgarman@gmail.com>
2015-11-22 16:20:19 +0000

    run.sh should install scrapy if not installed

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
+which scrapy > /dev/null || sudo pip install Scrapy
+
 >properties.json
 scrapy runspider scrape.py -t json -o properties.json
 #cat properties.json | jq '.'
